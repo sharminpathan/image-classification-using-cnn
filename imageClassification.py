4e67925339fde1146304677d2111cd9aa3884f1e
sharminpathan <sheryl.dr@gmail.com>
2016-10-03 02:33:35 -0400

    Update imageClassification.py

--- imageClassification.py
+++ imageClassification.py
@@ -1,7 +1,5 @@
-
 import urllib2
 import cStringIO
-import image
 import numpy as np
 import matplotlib
 import matplotlib.cm as cm
@@ -21,16 +19,20 @@ np.set_printoptions(threshold=np.inf)
 def average(pixel):
 	return 0.299*pixel[0] + 0.587*pixel[1] + 0.114*pixel[2]
 
-data = urllib2.urlopen("https://s3.amazonaws.com/eds-uga-csci8360/data/project3/metadata/X_train.txt").readLine()
-image = np.array(Image.open(cStringIO.StringIO(urllib2.urlopen("https://s3.amazonaws.com/eds-uga-csci8360/data/project3/images/"+data[0:5]+".png").read())))
+data = urllib2.urlopen("https://s3.amazonaws.com/eds-uga-csci8360/data/project3/metadata/X_train.txt")
+file = open('/home/sharmin/Desktop/data.txt','a')
 
-grey = np.zeros((32,32))
-for rownum in range(len(image)):
-	for colnum in range(len(image[rownum])):
-		grey[rownum][colnum] = average(im[rownum][colnum])
-plt.imshow(grey, cmap = matplotlib.cm.Greys_r)
-plt.show()
+for line in data:
+	image = np.array(Image.open(cStringIO.StringIO(urllib2.urlopen("https://s3.amazonaws.com/eds-uga-csci8360/data/project3/images/"+line[0:5]+".png").read())))
 
-flat_image = grey.ravel()
-vector = np.matrix(flat_image)
-print vector
+	grey = np.zeros((32,32))
+	for rownum in range(len(image)):
+		for colnum in range(len(image[rownum])):
+			grey[rownum][colnum] = average(image[rownum][colnum])
+			plt.imshow(grey, cmap = matplotlib.cm.Greys_r)
+
+			flat_image = grey.ravel()
+			vector = np.matrix(flat_image)
+	file.write(str(line[0:5]+' '+str(vector)+'\n'))
+
+file.close()
